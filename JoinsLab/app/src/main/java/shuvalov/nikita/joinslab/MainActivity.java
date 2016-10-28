9635a5fc3cb1f0376262da223c759fcfef3db4be
NikShuvalov <nshuvalov89@gmail.com>
2016-10-28 13:16:08 -0400

    Created function to populate database, and created buttons

--- JoinsLab/app/src/main/java/shuvalov/nikita/joinslab/MainActivity.java
+++ JoinsLab/app/src/main/java/shuvalov/nikita/joinslab/MainActivity.java
@@ -16,28 +16,25 @@ public class MainActivity extends AppCompatActivity {
     public void setUpDatabaseInfo(){
         EmployeeSQLOpenHelper openHelper = EmployeeSQLOpenHelper.getInstance(this);
 
-//        openHelper.insertEmployeeRow(new Employee("123-04-5678", "John", "Smith",  1973, "NY"));
-//        openHelper.insertEmployeeRow(new Employee("123-04-5679", "David", "McWill", 1982, "Seattle"));
-//
-//        openHelper.insertEmployeeRow(new Employee("123-04-5680", "Katerina", "Wise", 1973, "Boston"));
-//        Employee don = new Employee()
-//                David McWill 1982 Seattle
-//                Katerine Wise 1973 Boston
-//                Donald Lee 1992 London
-//                Gary Henwood 1987 Las Vegas
-//                Anthony Bright 1963 Seattle
-//                William Newey 1995 Boston
-//                Melony Smith 1970 Chicago
-//
-//
-//                123-04-5678 Fuzz 60 1
-//                GA 70 2
-//                Little Place 120 5
-//                Macy's 78 3
-//                New Life 65 1
-//                Blieve 158 6
-//                Macy's 200 8
-//                Stop 299 12
+        openHelper.insertEmployeeRow(new Employee("123-04-5678", "John", "Smith",  1973, "NY"));
+        openHelper.insertEmployeeRow(new Employee("123-04-5679", "David", "McWill", 1982, "Seattle"));
+        openHelper.insertEmployeeRow(new Employee("123-04-5680", "Katerina", "Wise", 1973, "Boston"));
+        openHelper.insertEmployeeRow(new Employee("123-04-5681", "Donald", "Lee", 1992, "London"));
+        openHelper.insertEmployeeRow(new Employee("123-04-5682", "Gary", "Henwood", 1987, "Las Vegas"));
+        openHelper.insertEmployeeRow(new Employee("123-04-5683", "Anthony", "Bright", 1963, "Seattle"));
+        openHelper.insertEmployeeRow(new Employee("123-04-5684", "William", "Newey", 1995, "Boston"));
+        openHelper.insertEmployeeRow(new Employee("123-04-5685", "Melony", "Smith", 1970, "Chicago"));
+
+        openHelper.insertJobRow(new Job("123-04-5678","Fuzz", 60, 1));
+        openHelper.insertJobRow(new Job("123-04-5679","GA", 70, 2));
+        openHelper.insertJobRow(new Job("123-04-5680","Little Place", 120, 5));
+        openHelper.insertJobRow(new Job("123-04-5681","Macys", 78, 3));
+        openHelper.insertJobRow(new Job("123-04-5682","New Life", 65, 1));
+        openHelper.insertJobRow(new Job("123-04-5683","Believe", 158, 6));
+        openHelper.insertJobRow(new Job("123-04-5684","Macys", 200, 8));
+        openHelper.insertJobRow(new Job("123-04-5685","Stop", 299, 12));
+
+
     }
 }
 
